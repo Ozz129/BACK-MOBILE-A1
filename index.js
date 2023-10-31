a4b86cf7cf5615e8ee807b45740ca493e5e7805c
Fabian Martinez Hoyos <ozz@Fabians-MacBook-Pro.local>
2023-10-31 07:55:29 -0500

    endpoints added to retrieve general info, save users and login process

--- index.js
+++ index.js
@@ -28,3 +28,46 @@ connectToMongo().then(() => {
     console.log('Servidor corriendo en el puerto 3000');
   });
 });
+
+app.post('/users', async (req, res, next) => {
+    try {
+        const newUser = req.body
+        const collection = db.collection('users');
+        const documentos = await collection.insertOne(newUser);
+        res.send(documentos);
+    } catch (error) {
+        next(error);
+    }
+});
+
+app.post('/login', async (req, res, next) => {
+    try {
+        const { user, password } = req.body
+        const collection = db.collection('users');
+        const results = await collection.findOne({ email: user });
+
+        if (!results || results.password !== password) {
+            return res.status(404).send('Usuario o contraseña incorrectos.');
+        }
+
+        res.send({
+            msg: 'Bienvenido'
+        });
+
+    } catch (error) {
+        next(error);
+    }
+});
+
+app.get('/general', async(req, res, next) => {
+    const { type } = req.query
+    const collection = db.collection('general');
+    const results = await collection.findOne({ type });
+
+    if (!results) {
+        return res.status(404).send('Contenido no encontrado');
+    }
+
+    res.send(results)
+
+})
